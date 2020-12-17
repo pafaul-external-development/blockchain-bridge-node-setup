d141cd8ae8dc9799d92ae1b6e4c078c8282b5e59
Paul Mikhaylov <paulmihailov2013@gmail.com>
2020-12-17 18:40:43 +0300

    Minor modifications for test case

--- node_api/modules/test_post_requests.js
+++ node_api/modules/test_post_requests.js
@@ -2,22 +2,33 @@ const AxiosInstance = require('./request_instance');
 const EndPointRequests = require('./endpoint_requests');
 const config = require('../config');
 
-let address = 'http://' + config.btcv.address + ':' + config.btcv.port;
-let user = config.btcv.auth.user;
-let passwd = config.btcv.auth.passwd;
-let instance = new AxiosInstance(address, user, passwd);
+function create_instance(config) {
+    let address = 'http://' + config.address + ':' + config.port;
+    let user = config.auth.user;
+    let passwd = config.auth.passwd;
+    let instance = new AxiosInstance(address, user, passwd);
+    return instance;
+}
 
-let requests = new EndPointRequests(instance);
+let btcv_request_instance = create_instance(config.btcv);
+let gleecs_request_instance = create_instance(config.gleecs);
+
+let btcv_endpoint = new EndPointRequests(btcv_request_instance);
+let gleecs_endpoint = new EndPointRequests(gleecs_request_instance);
+
+async function test_wallet_creation(request_instance, wallet_name) {
+    let data = await request_instance.createWallet(wallet_name);
+    console.log(data);
+    let balance = await request_instance.getBalance(wallet_name);
+    console.log(balance);
+}
 
 async function main() {
-    let data = await requests.createWallet('1234');
-    console.log(data);
-    let balance = await requests.getBalance('1234');
-    console.log(balance);
+    await test_wallet_creation(btcv_endpoint);
+    await test_wallet_creation(gleecs_endpoint);
+    process.exit(0)
 }
 
 main();
 
 // instance.make_post_request('', 'getnewaddress', []).then(console.log).catch(console.log)
-
-
