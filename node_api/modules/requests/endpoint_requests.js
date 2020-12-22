4a441fdd35ee0ff595716db6137fbc2dd0923831
Paul Mikhaylov <paulmihailov2013@gmail.com>
2020-12-23 00:51:04 +0300

    fix #8

--- node_api/modules/requests/endpoint_requests.js
+++ node_api/modules/requests/endpoint_requests.js
@@ -53,7 +53,7 @@ class EndPointRequests {
         if (!walletExists) {
             let walletId = getWalletId(currency, userId);
 
-            let walletData = await this[currency].instance.createWallet(walletId);
+            let walletData = await this[currency].requests.createWallet(walletId);
             if (walletData) {
                 let wallet = await this.database.safeAddKeyVault(userId, currency, walletData[0], walletId);
                 if (wallet) {}
@@ -72,7 +72,7 @@ class EndPointRequests {
         let existingWallets = await this.database.getAllKeyVaultsByUid(userId);
         let walletInfo = [];
         for (const walletData of existingWallets) {
-            let wallet = await this[walletData.wallet_currency].instance.getWallet(walletData.wallet_id);
+            let wallet = await this[walletData.wallet_currency].requests.getWallet(walletData.wallet_id);
             walletInfo.push([walletData.wallet_currency, wallet, walletData.pub_key]);
         }
         return walletInfo;
@@ -85,7 +85,7 @@ class EndPointRequests {
     async getHistory(walletId) {
         let wallet = await this.database.getKeyVaultByWalletId(walletId);
         if (currency) {
-            let walletInfo = await this[wallet.wallet_currency].instance.getWallet(wallet.walletId);
+            let walletInfo = await this[wallet.wallet_currency].requests.getWallet(wallet.walletId);
             return walletInfo;
         }
         return 
@@ -99,7 +99,7 @@ class EndPointRequests {
         let wallets = await this.database.getAllKeyVaultsByUserId(userId);
         let history = [];
         for (const walletData of wallets) {
-            let walletHistory = await this[walletData.wallet_currency].instance.getHistory(walletData.wallet_id);
+            let walletHistory = await this[walletData.wallet_currency].requests.getHistory(walletData.wallet_id);
             history.push([walletData.wallet_currency, walletHistory]);
         }
         return history;
@@ -112,7 +112,7 @@ class EndPointRequests {
      */
     async getTxData(walletId, txId) {
         let wallet = await this.database.getKeyVaultByWalletId(walletId);
-        let txData = await this[wallet.name].instance.getTxData(wallet.id, txId);
+        let txData = await this[wallet.name].requests.getTxData(wallet.id, txId);
         return txData;
     }
 
@@ -127,7 +127,7 @@ class EndPointRequests {
     async createTx(currency, userId, to, amount, callback) {
         let wallet = await this.database.getKeyVault(userId, currency);
         if (wallet) {
-            let txData = await this[wallet.name].instance.createTx(wallet.id, to, String(amount));
+            let txData = await this[wallet.name].requests.createTx(wallet.id, to, String(amount));
             if (txData) {
                 if (txData.txId) {
                     // был получен txId и данные по транзакции на текущий момент
@@ -150,7 +150,7 @@ class EndPointRequests {
     async getTxComission(currency, userId, to, amount) {
         let wallet = this.database.getKeyVault(userId, currency);
         let confirmationBlocks = BlockchainConfig[currency].confirmationBlocks;
-        let fee = await this[wallet.name].instance.getTxComission(confirmationBlocks);
+        let fee = await this[wallet.name].requests.getTxComission(confirmationBlocks);
         return fee;
     }
 }
