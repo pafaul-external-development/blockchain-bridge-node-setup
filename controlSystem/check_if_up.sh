ff314f661ccca0c4ec9c52ead4c5f89f1aee1010
Paul Mikhaylov <paulmihailov2013@gmail.com>
2020-12-27 23:01:39 +0300

    Cron setup created

--- controlSystem/check_if_up.sh
+++ controlSystem/check_if_up.sh
@@ -2,10 +2,59 @@
 
 DIRROOT=$(cd $(dirname $0) && pwd)
 
-GLEECS_NODE_IP=
-GLEECS_PORT=
-BTCV_NODE_IP=
-BTCV_PORT=
+display_help() {
+    echo "Bash script to backup database"
+    echo
+    echo "Possible arguments:"
+    echo "  -gi, --gleec-ip       Gleec node ip address"
+    echo "  -gp, --gleec-port     Gleec node ssh port"
+    echo "  -bi, --btcv-ip        Btcv node ip address"
+    echo "  -bp, --btcv-port      Btcv node ssh port"
+    echo "If no arguments specified, silently exits."
+}
+
+parse_input_parameters() {
+    while [ 1 -eq 1 ]
+    do
+        if [ $# -eq 0 ]
+        then
+            break
+        fi
+
+        case $1 in          
+            -gi | --gleec-ip)
+                GLEECS_NODE_IP=${2}
+                shift 2
+            ;;
+
+            -gp | --gleec-port)
+                GLEECS_PORT=${2}
+                shift 2
+            ;;
+
+            -bi | --btcv-ip)
+                BTCV_NODE_IP=${2}
+                shift 2
+            ;;
+
+            -bp | --btcv-port)
+                BTCV_PORT=${2}
+                shift 2
+            ;;
+
+            -h | --help)
+                display_help
+                exit 0
+            ;;
+            
+            *)
+                echo "Unknown parameter"
+                display_help
+                exit 1
+                ;;
+        esac
+    done
+}
 
 loop() {
     ping -c 4 -p ${BTCV_PORT} ${BTCV_NODE_IP}
@@ -14,12 +63,11 @@ loop() {
     gleecs_status=$?
 }
 
-while [ -f $DIRROOT/.look_for_nodes ]
-do
-    loop
-    status_string="[$(date '+%d-%m-%y-%H:%M:%S')]:"
-    [ $btcv_status ] && status_string="${status_string} btcv->ok" || status_string="${status_string} btcv->fail"
-    [ $gleecs_status ] && status_string="${status_string} gleecs->ok" || status_string="${status_string} gleecs->fail"
-    echo "${status_string}" >> ${DIRROOT}/.nodes_status
-    sleep 5
-done
+parse_input_parameters $@
+
+loop
+
+status_string="[$(date '+%d-%m-%y-%H:%M:%S')]:"
+[ $btcv_status ] && status_string="${status_string} btcv->ok" || status_string="${status_string} btcv->fail"
+[ $gleecs_status ] && status_string="${status_string} gleecs->ok" || status_string="${status_string} gleecs->fail"
+echo "${status_string}" >> ${DIRROOT}/.nodes_status
--- controlSystem/setup_background_tasks.sh
+++ controlSystem/setup_background_tasks.sh
@@ -14,11 +14,15 @@ echo_and_exit() {
 }
 
 display_help() {
-    echo "Bash script to backup database"
+    echo "Bash script to backup database and check if nodes are up"
     echo
     echo "Possible arguments:"
     echo "  -db, --database      Database filename"
     echo "  -u, --user           User which will do backups"
+    echo "  -gi, --gleec-ip      Gleec node ip address"
+    echo "  -gp, --gleec-port    Gleec node ssh port"
+    echo "  -bi, --btcv-ip       Btcv node ip address"
+    echo "  -bp, --btcv-port     Btcv node ssh port"
     echo "If no arguments specified, silently exits."
 }
 
@@ -38,6 +42,26 @@ parse_input_parameters() {
 
             -u | --user)
                 BACKUP_USER=$2
+                shift 2
+            ;;
+
+            -gi | --gleec-ip)
+                GLEECS_NODE_IP=${2}
+                shift 2
+            ;;
+
+            -gp | --gleec-port)
+                GLEECS_PORT=${2}
+                shift 2
+            ;;
+
+            -bi | --btcv-ip)
+                BTCV_NODE_IP=${2}
+                shift 2
+            ;;
+
+            -bp | --btcv-port)
+                BTCV_PORT=${2}
                 shift 2
             ;;
 
@@ -64,8 +88,9 @@ setup_backup() {
 cat > ${CRON_FILE} <<EOF
 SHELL=/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
-* * * * * ${BACKUP_USER} cd ${DIRROOT} && /bin/bash ${DIRROOT}/backup_database.sh -db ${DATABASE_FILE}
-* * * * * ${BACKUP_USER} cd ${DIRROOT} && /bin/bash ${DIRROOT}/backup_wallets.sh
+0   * * * * ${BACKUP_USER} cd ${DIRROOT} && /bin/bash ${DIRROOT}/backup_database.sh -db ${DATABASE_FILE}
+0   * * * * ${BACKUP_USER} cd ${DIRROOT} && /bin/bash ${DIRROOT}/backup_wallets.sh
+*/5 * * * * ${BACKUP_USER} cd ${DIRROOT} && /bin/bash ${DIRROOT}/check_if_up.sh -gi ${GLEECS_NODE_IP} -gp ${GLEECS_PORT} -bi ${BTCV_NODE_IP} -bp ${BTCV_PORT}
 
 EOF
 }
