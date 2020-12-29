14ed68a177c9ff994d888e3f40cfe3b0ccce5509
Paul Mikhaylov <paulmihailov2013@gmail.com>
2020-12-29 21:56:30 +0300

    fix of control_structure

--- controlSystem/scripts/configure_node.sh
+++ controlSystem/scripts/configure_node.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
 apt update
 
 apt install -y software-properties-common
 add-apt-repository -y universe
-sudo add-apt-repository -y ppa:bitcoin/bitcoin
+add-apt-repository -y ppa:bitcoin/bitcoin
 apt update
-sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
+apt-get install -y libdb4.8-dev libdb4.8++-dev
